3991b775933c5c8efbfa86c07f0b57f7c5ff2c20
潘倪楠 <982397045@qq.com>
2019-09-20 10:24:19 +0800

    console

--- ex/ex2.py
+++ ex/ex2.py
@@ -1,11 +1,31 @@
 import requests
-import urllib.request
+from urllib import request
 import re  # 正则匹配目标文件
 import os
-import urllib
 
-pic = requests.get('http://pics.sc.chinaz.com/files/pic/pic9/201801/zzpic9947.jpg')
+# pic = requests.get('http://pics.sc.chinaz.com/files/pic/pic9/201801/zzpic9947.jpg')
+from requests import HTTPError
 
 if __name__ == '__main__':
-    with open('picfile.jpg', 'wb') as f:
-        f.write(pic.content)
+    # 1
+    # req = urllib.request.Request('http://www.baidu.com/')
+    # response = urllib.request.urlopen(req)
+    # the_page = response.read()
+    # print(the_page)
+    # 2
+    # response = urllib.request.urlopen('http://www.baidu.com/')
+    # html = response.read()
+
+    req = request.Request('http://www.douban.com/')
+    # iphone 6
+    req.add_header('User-Agent',
+                   'Mozilla/6.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) '
+                   'Version/8.0 Mobile/10A5376e Safari/8536.25')
+    # chrome
+    req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                                 'Chrome/76.0.3809.100 Safari/537.36')
+    with request.urlopen(req) as f:
+        print('Status:', f.status, f.reason)
+        for k, v in f.getheaders():
+            print('%s: %s' % (k, v))
+        print('Data:', f.read().decode('utf-8'))
--- practice/打印三角形.py
+++ practice/打印三角形.py
@@ -12,12 +12,12 @@
 
 def print_l():
     row = int(input('请输入需要打印的三角形的行数: '))
-    print('-' * 40 + '开始打印' + '-' * 40)
+    print('-' * 20 + '开始打印' + '-' * 20)
     for r in range(row):  # 控制行数
         for i in range(r + 1):  # 控制打印*数
             print('*', end='')
         print()
-    print('-' * 40 + '打印结束' + '-' * 40)
+    print('-' * 20 + '打印结束' + '-' * 20)
 
 
 '''
@@ -30,7 +30,7 @@ def print_l():
 
 def print_r():
     row = int(input('请输入需要打印的三角形的行数: '))
-    print('-' * 40 + '开始打印' + '-' * 40)
+    print('-' * 20 + '开始打印' + '-' * 20)
     for r in range(row):  # 控制行数
         for i in range(row):  # 控制打印*数
             if row - r - 1 > i:
@@ -38,7 +38,7 @@ def print_r():
             else:
                 print('*', end='')
         print()
-    print('-' * 40 + '打印结束' + '-' * 40)
+    print('-' * 20 + '打印结束' + '-' * 20)
 
 
 if __name__ == '__main__':
