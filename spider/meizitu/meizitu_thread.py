75a76a9c9aacee5fdf93681c96ebc95f9650bb11
潘倪楠 <982397045@qq.com>
2020-08-04 21:09:31 +0800

    1

--- spider/meizitu/meizitu_thread.py
+++ spider/meizitu/meizitu_thread.py
@@ -5,7 +5,6 @@ from threading import Thread
 
 import requests
 from bs4 import BeautifulSoup
-from requests import ReadTimeout
 
 
 def mkdir(path):  # 这个函数创建文件夹
@@ -44,18 +43,9 @@ def all_url(page_url, uqueue):
     # 最后一页页码数
     last_num = last_a.find('span').text
     for page in range(1, int(last_num) + 1):
-        page_url = page_url + '/' + str(page)
-        uqueue.put(path + '#' + page_url)
-
-
-def html(href):  # 这个函数是处理套图地址获得图片的页面地址
-    html_2 = request(href)
-    headers['referer'] = href
-    max_span = BeautifulSoup(html_2.text, 'lxml').find(
-        'div', class_='pagenavi').find_all('span')[-2].get_text()
-    for page in range(1, int(max_span) + 1):
-        page_url = href + '/' + str(page)
-        img(page_url)  # 调用img函数
+        img_page = page_url + '/' + str(page)
+        print(img_page)
+        uqueue.put(path + '#' + img_page)
 
 
 def img(in_q):  # 这个函数处理图片页面地址获得图片的实际地址
@@ -82,7 +72,7 @@ def request(request_url):
     try:
         content = requests.get(request_url, headers=headers, timeout=5)
         return content
-    except (ConnectionError, ReadTimeout):
+    except Exception as e:
         print('Crawling Failed', request_url)
         return None
 
@@ -97,13 +87,18 @@ if __name__ == '__main__':
     thread_1 = Thread(target=all_url, args=(url, queue,))
     thread_1.daemon = True  # 随主线程退出而退出
     thread_1.start()
-    time.sleep(1)
+    thread_1.join()
+
+    works = []
     for index in range(5):
         # 获取图片内容并保存
         thread = Thread(target=img, args=(queue,))
         thread.daemon = True  # 随主线程退出而退出
         thread.start()
+        works.append(thread)
+    for work in works:
+        work.join()
 
-    queue.join()  # 队列消费完 线程结束
+    # queue.join()  # 队列消费完 线程结束
     end = time.time()
     print('总耗时：%s' % (end - start))
