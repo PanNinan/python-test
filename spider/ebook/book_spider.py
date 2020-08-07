5c20840d2a96f299c73ec8201f6bdf1a3da80ec8
PanNiNan <982397045@qq.com>
2020-08-07 22:14:18 +0800

    1

--- spider/ebook/book_spider.py
+++ spider/ebook/book_spider.py
@@ -1,52 +1,69 @@
 # encoding = utf-8
+import random
+import threading
+import time
 from queue import Queue
 
+import redis
 import requests
 from bs4 import BeautifulSoup
 
+# 连接池
+conn_pool = redis.ConnectionPool(host='127.0.0.1', port=6379, password='', decode_responses=True)
+
 
 # 获取小说所有章节分页html地址
-def get_chapter_page_list(html):
+def get_chapter_page_list(html, chapter_queue):
+    print('章节URL采集线程开启...')
     target_html = get_page(html)
     soup = get_soup(target_html)
     try:
         page_select = soup.find_all(name="option")
+        chapter_page = []
         for option in page_select:
             href = domain + option['value']
             chapter_page.append(href)
-            # chapter_page_queue.put(href)
-        return chapter_page
+        for item in chapter_page:
+            get_all_chapter(item, chapter_queue)
+        print('章节URL采集线程结束...')
     except Exception as e:
         print('解析所有章节列表网址出错:' + str(e))
 
 
 # 获取所有章节
-def get_all_chapter(url):
-    with open('q.txt', 'w') as file:
-        chapter_html = get_page(url)
-        soup = get_soup(chapter_html)
-        uls = soup.find_all(name="ul")
-        tag_a = uls[4].find_all(name="a")
-        for item in tag_a:
-            chapter_url = domain + item['href']
-            # chapter_queue.put(chapter_url)
-            file.write(chapter_url + "\n")
-    file.close()
+def get_all_chapter(url, chapter_queue):
+    chapter_html = get_page(url)
+    soup = get_soup(chapter_html)
+    uls = soup.find_all(name="ul")
+    tag_a = uls[4].find_all(name="a")
+    rs = redis.Redis(connection_pool=conn_pool)
+    for item in tag_a:
+        chapter_url = domain + item['href']
+        # get_detail(chapter_url)
+        print(chapter_url)
+        chapter_queue.put(chapter_url)  # 每章节url放入队列
 
 
-# 获取章节内容
-def get_detail(html):
+# 获取小说内容
+def get_detail(chapter_queue):
     try:
-        text = get_page(html)
-        soup = get_soup(text)
-        pb_next = soup.find(name="h1", id="chaptertitle").get_text(strip=True)
-        max = pb_next[-3]
-        if int(max) > 1:  # 有分页内容
-            for i in range(int(max)):
-                i_html = html.replace(".html", "") + "_" + str(i + 1) + ".html"
-                content = get_content(i_html)
-        else:
-            content = get_content(html)
+        while chapter_queue.empty() is not True:
+            html = chapter_queue.get(timeout=3)
+            text = get_page(html)
+            soup = get_soup(text)
+            pb_next = soup.find(name="h1", id="chaptertitle").get_text(strip=True)
+            max = pb_next[-3]
+            rs = redis.Redis(connection_pool=conn_pool)
+            if int(max) > 1:  # 有分页内容
+                for i in range(int(max)):
+                    i_html = html.replace(".html", "") + "_" + str(i + 1) + ".html"
+                    content = get_content(i_html)
+                    score = get_score_from_url(i_html)
+                    rs.zadd('ebook', {content: score})
+            else:
+                content = get_content(html)
+                score = get_score_from_url(html)
+                rs.zadd('ebook', {content: score})
     except Exception as e:
         print('出错' + str(e))
 
@@ -69,10 +86,18 @@ def get_content(html):
     soup = get_soup(text)
     div = soup.find(name="div", class_="novelcontent")
     ps = div.find_all(name="p")
-    title = ps[0].get_text() + "\n"
+    # title = ps[0].get_text() + "\n"
     content = ps[1].get_text() + "\n"
-    # print(title, content)
-    # exit()
+    return content
+
+
+def get_score_from_url(url):
+    tail_str = url.split('/')[-1].split('.')[0]
+    if tail_str.find('_') != -1:
+        url_number = tail_str.split('_')
+        return int(url_number[0]) + int(url_number[1])
+    else:
+        return int(tail_str)
 
 
 domain = 'https://m.vcxsw.com/'
@@ -83,16 +108,36 @@ headers = {
     'Referrer': 'https://m.vcxsw.com'
 }
 txt_content = {}  # 存储小说所有内容
-chapter_page_queue = Queue()
-chapter_queue = Queue()
-chapter_page = []
 chapters = []
 
 thread_list = []
 if __name__ == '__main__':
-    accept_chapters = get_chapter_page_list(target_url)
-    for item in accept_chapters:
-        get_all_chapter(item)
+    rs = redis.Redis(connection_pool=conn_pool)
+    data = rs.zrange('ebook', 0, -1, desc=False, withscores=True)
+    with open('xxx.txt', 'a', encoding="utf-8") as file:
+        for item in data:
+            file.write(item[0])
+    file.close()
+    exit()
+
+    begin = time.time()
+    spider_threads = []
+    chapter_queue = Queue()
+    T = threading.Thread(target=get_chapter_page_list, args=(target_url, chapter_queue,))
+    T.start()
+    T.join()
+    for i in range(10):
+        t = threading.Thread(target=get_detail, args=(chapter_queue,))
+        spider_threads.append(t)
+    for t in spider_threads:
+        t.start()
+    for t in spider_threads:
+        t.join()
+    print('\n子线程运行完毕')
+    end = time.time()
+    print('下载完毕，总耗时', end - begin, '秒')
+    # with open('1.txt', 'a', encoding="utf-8") as file:
+    # file.close()
     # begin = time.time()
     # chaptername = Queue()  # 存放小说章节地址
     # for id in range(11):
@@ -106,7 +151,7 @@ if __name__ == '__main__':
     #     t.join()
     # print('\n子线程运行完毕')
     # txtcontent1 = sorted(txt_content)
-    # file = codecs.open('q.txt', 'w', 'utf-8')  # 小说存放在本地的地址
+    # file = codecs.open('page_url.txt', 'w', 'utf-8')  # 小说存放在本地的地址
     # chaptercount = len(chaptername)
     # 
     # # 写入文件中
